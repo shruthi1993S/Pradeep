7f36819c2810ddebaaf8cceb2eb71ffa25f7e464
shruthi1993s <shruthiji06@gmail.com>
2024-03-18 00:57:13 +0530

    examples

---
+++ Project Test/index.js
@@ -0,0 +1,31 @@
+console.log("Pradeep")
+/*
+var weekdays = ["m","t","w","thu","fri"]
+weekdays.splice(3,0,"yellow")
+
+var color = ["red","green","blue"]
+
+color.splice(1,0,"yellow","pink") //position, add(0) or replace(1), items
+
+console.log(color)
+
+console.log(weekdays)
+*/
+
+
+/*
+//functions
+var value = prompt("Enter your bill Amount : ")
+var percent = prompt("Enter your GST Percent : ")
+
+total = calculateGST(value,percent);
+alert("The Net Value after GST is : "+total);
+
+function calculateGST(value,percent)
+{
+    var net = parseInt(value) + (value * percent / 100);
+    return net
+}
+
+*/
+
